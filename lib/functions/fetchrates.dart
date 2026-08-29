5e6e486c0d7c66a270e61b39625fdd4cfe7389a1
FAHIMSHAKIL102 <fahimshakil102@gmail.com>
2026-08-29 12:48:19 +0600

    feat: functions

--- lib/functions/fetchrates.dart
+++ lib/functions/fetchrates.dart
@@ -1,5 +1,47 @@
+import 'package:flutter_currency_convert_app/models/allcurrencies.dart';
+import 'package:flutter_currency_convert_app/models/ratesmodel.dart';
+import 'package:flutter_currency_convert_app/utils/key.dart';
+import 'package:http/http.dart' as http;
 
+Future<RatesModel> fetchrates() async {
+  var response = await http.get(
+    Uri.parse(
+      'https://openexchangerates.org/api/latest.json?base=USD&app_id=' + key,
+    ),
+  );
+  final result = ratesModelFromJson(response.body);
+  return result;
+}
 
+Future<Map> fetchcurrencies() async {
+  var response = await http.get(
+    Uri.parse(
+      'https://openexchangerates.org/api/currencies.json?app_id=' + key,
+    ),
+  );
+  final allCurrencies = allCurrenciesFromJson(response.body);
+  return allCurrencies;
+}
 
+String convertusd(Map exchangeRates, String usd, String currency) {
+  String output =
+      ((exchangeRates[currency] * double.parse(usd)).toStringAsFixed(2))
+          .toString();
+  return output;
+}
 
-Future<RatesModel> fetchrates() async{}
+String convertany(
+  Map exchangeRates,
+  String amount,
+  String currencybase,
+  String currencyFinal,
+) {
+  String output =
+      (double.parse(amount) /
+              exchangeRates[currencybase] *
+              exchangeRates[currencyFinal])
+          .toStringAsFixed(2)
+          .toString();
+
+  return output;
+}
