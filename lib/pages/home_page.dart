2d275b47a7acd79ca567c498003223f240b4478e
FAHIMSHAKIL102 <fahimshakil102@gmail.com>
2026-08-29 19:37:29 +0600

    feat: home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_currency_convert_app/components/any_to_any.dart';
+import 'package:flutter_currency_convert_app/components/usd_to_any.dart';
+import 'package:flutter_currency_convert_app/functions/fetchrates.dart';
+import 'package:flutter_currency_convert_app/models/ratesmodel.dart';
 
 class HomePage extends StatefulWidget {
   const new({super.key});
@@ -8,6 +12,21 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // initial variables
+  late Future<RatesModel> result;
+  late Future<Map> allCurrencies;
+  final formkey = GlobalKey<FormState>();
+
+  // getting ratesmodel and all currencies
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      result = fetchrates();
+      allCurrencies = fetchcurrencies();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     var h = MediaQuery.of(context).size.height;
@@ -17,7 +36,43 @@ class _HomePageState extends State<HomePage> {
         title: Text('Open Exchange'),
         backgroundColor: Colors.pink,
       ),
-      body: SingleChildScrollView(child: Center()),
+      body: SingleChildScrollView(
+        child: Form(
+          key: formkey,
+          child: FutureBuilder<RatesModel>(
+            future: result,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(child: CircularProgressIndicator());
+              }
+              return Center(
+                child: FutureBuilder<Map>(
+                  future: allCurrencies,
+                  builder: (context, currencysnapshot) {
+                    if (currencysnapshot.connectionState ==
+                        ConnectionState.waiting) {
+                      return Center(child: CircularProgressIndicator());
+                    }
+                    return Column(
+                      mainAxisAlignment: .center,
+                      children: [
+                        UsdToAny(
+                          rates: snapshot.data!.rates,
+                          currencies: currencysnapshot.data!,
+                        ),
+                        AnyToAny(
+                          rates: snapshot.data!.rates,
+                          currencies: currencysnapshot.data!,
+                        ),
+                      ],
+                    );
+                  },
+                ),
+              );
+            },
+          ),
+        ),
+      ),
     );
   }
 }
